d92f63ee84e78de5e84df05446b3e59951039ebc
Piotr Filarski <piotrfilarski@gmail.com>
2021-06-04 15:51:30 +0200

    Add project files.

---
+++ FSWebAppSimple/FsDBContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using FSWebAppSimple.Models;
+using System.Data.Entity;
+
+
+namespace FSWebAppSimple
+{
+    public class FsDBContext : DbContext
+    {
+        public FsDBContext() : base("FsDBContext")
+        {
+
+        }
+        public DbSet<FinancialStatement> FinancialStatements { get; set; }
+    }
+}
---
+++ FSWebAppSimple/Models/FinancialStatementFile.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FSWebAppSimple.Models
+{
+    public class FinancialStatementFile
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public int Income { get; set; }
+        public int Age { get; set; }
+        public HttpPostedFileBase Photo { get; set; }
+    }
+}
